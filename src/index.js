d4888d7903b87a017cbac20577d6aab337de193b
harshit <harshitpurohit@gkmit.co>
2024-10-17 19:53:15 +0530

    feat: create database connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/users.route.js');
-
+const {sequelize} = require('./models/index.js');
+// const { sequelize } = require('./models');
 app.use(express.json());
 
+
+
 app.use('/api', userRoutes);
+
+
+const startServer = async function () {
+  try {
+    await sequelize.authenticate();
+    console.log('database connected');
+
+    // const serverPort = process.env.PORT || 3000;
+    // app.listen(serverPort);
+    // console.log(`--- Server started on ${serverPort} ---\n\n`);
+  } catch (err) {
+    // console.log('server setup failed', err);
+    console.log('Database connection failed', err);
+
+    console.log('Error: ', err.message);
+  }
+};
+
+startServer();
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
